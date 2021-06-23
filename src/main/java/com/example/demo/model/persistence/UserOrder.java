bc59c03be0cd08ad34ab6a13183b229da66954a0
“Alan <alan.hillgemann@anz.com>
2021-06-23 16:09:20 +1000

    feat: implement security

--- src/main/java/com/example/demo/model/persistence/UserOrder.java
+++ src/main/java/com/example/demo/model/persistence/UserOrder.java
@@ -1,62 +1,59 @@
 package com.example.demo.model.persistence;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "user_order")
 public class UserOrder {
 
+	@Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@JsonProperty
+    @JsonProperty
+    private Long id;
+
 	@Column
-	private Long id;
-	
+    @JsonProperty
 	@ManyToMany(cascade = CascadeType.ALL)
-	@JsonProperty
-	@Column
     private List<Item> items;
-	
+
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
+    @JsonProperty
 	@ManyToOne
-	@JoinColumn(name="user_id", nullable = false, referencedColumnName = "id")
-	@JsonProperty
-    private User user;
-	
-	@JsonProperty
+	private User user;
+
 	@Column
-	private BigDecimal total;
+    @JsonProperty
+    private BigDecimal total;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public List<Item> getItems() {
-		return items;
-	}
+    public List<Item> getItems() {
+        return items;
+    }
 
-	public void setItems(List<Item> items) {
-		this.items = items;
-	}
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
 
 	public User getUser() {
 		return user;
@@ -64,14 +61,6 @@ public class UserOrder {
 
 	public void setUser(User user) {
 		this.user = user;
-	}
-	
-	public BigDecimal getTotal() {
-		return total;
-	}
-
-	public void setTotal(BigDecimal total) {
-		this.total = total;
 	}
 
 	public static UserOrder createFromCart(Cart cart) {
@@ -81,5 +70,4 @@ public class UserOrder {
 		order.setUser(cart.getUser());
 		return order;
 	}
-	
 }
